44bf2d9f27c3705164ac80b2344f0f72444d5b33
Warren <deed821@gmail.com>
2014-04-21 11:17:21 +1000

    fixed up ondemand plus governor for our device

--- drivers/cpufreq/cpufreq_ondemandplus.c
+++ drivers/cpufreq/cpufreq_ondemandplus.c
@@ -101,19 +101,19 @@ static u64 allowed_min;
 #define DEFAULT_MAX_FREQ 2265600
 static u64 allowed_max;
 
-#define DEFAULT_INTER_HIFREQ 1728000
+#define DEFAULT_INTER_HIFREQ 1890000
 static u64 inter_hifreq;
 
-#define DEFAULT_INTER_LOFREQ 300000
+#define DEFAULT_INTER_LOFREQ 384000
 static u64 inter_lofreq;
 
-#define SUSPEND_FREQ 300000
+#define SUSPEND_FREQ 384000
 static u64 suspend_frequency;
 
 #define DEFAULT_INTER_STAYCYCLES 2
 static unsigned long inter_staycycles;
 
-#define DEFAULT_STAYCYCLES_RESETFREQ 652800
+#define DEFAULT_STAYCYCLES_RESETFREQ 648000
 static u64 staycycles_resetfreq;
 
 #define DEFAULT_IO_IS_BUSY 0
