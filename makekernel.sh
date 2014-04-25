00d7a8c91eadc06548c04d8ad6b2bddbae900680
Warren <deed821@gmail.com>
2014-04-26 02:18:02 +1000

    update kernel build script

--- makekernel.sh
+++ makekernel.sh
@@ -1,3 +1,3 @@
 cp ./arch/arm/configs/opensemc_fuji_hikari_row_defconfig .config
-ARCH=arm CROSS_COMPILE=~/github/kernel/cortex-a8/bin/arm-cortex_a8-linux-gnueabi- make -j8 menuconfig
-ARCH=arm CROSS_COMPILE=~/github/kernel/cortex-a8/bin/arm-cortex_a8-linux-gnueabi- make -j8
+ARCH=arm CROSS_COMPILE=~/github/kernel/prebuilt/arm-cortex_a8-linux-gnueabi-linaro_4.7.4-2014.01/bin/arm-cortex_a8-linux-gnueabi- make -j8 menuconfig
+ARCH=arm CROSS_COMPILE=~/github/kernel/prebuilt/arm-cortex_a8-linux-gnueabi-linaro_4.7.4-2014.01/bin/arm-cortex_a8-linux-gnueabi- make -j8
